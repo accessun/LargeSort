9bd3128636cb1a1007f922340587112502800e81
Xin Sun <accessun@hotmail.com>
2017-03-01 16:55:38 +0800

    rewriting of sorter and validator using NIO and Java 8 API

--- src/io/github/accessun/largesort/operator/DataSorter.java
+++ src/io/github/accessun/largesort/operator/DataSorter.java
@@ -1,15 +1,13 @@
 package io.github.accessun.largesort.operator;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.github.accessun.largesort.comparator.NameComparator;
 import io.github.accessun.largesort.exception.DataFormatException;
@@ -22,7 +20,7 @@ public class DataSorter {
      * Sort the data in a file. Note that, by default, the sorting order is
      * specified by the <tt>NameComparator</tt>. And the file will be
      * overwritten by the sorted data.
-     * 
+     *
      * @param pathname
      *            the path of the file to be sorted
      * @throws IOException
@@ -35,7 +33,7 @@ public class DataSorter {
     /**
      * Sort the data in a file. Note that, by default, the file will be
      * overwritten by the sorted data.
-     * 
+     *
      * @param pathname
      *            the path of the file to be sorted
      * @param comp
@@ -48,7 +46,7 @@ public class DataSorter {
 
     /**
      * Sort the data in a file.
-     * 
+     *
      * @param pathname
      *            the path of the file to be sorted
      * @param targetPath
@@ -61,25 +59,19 @@ public class DataSorter {
      */
     public void sort(String pathname, String targetPath, Comparator<Record> comp)
             throws IOException, DataFormatException {
-        File file = new File(pathname);
-        FileReader fr = new FileReader(file);
-        BufferedReader reader = new BufferedReader(fr);
-        List<Record> records = new ArrayList<>();
-        String line;
-        Record record;
-        while ((line = reader.readLine()) != null) {
-            record = DataMappingUtils.mapToRecord(line);
-            records.add(record);
-        }
-        reader.close();
+
+        List<Record> records = Files.readAllLines(Paths.get(pathname)).stream().map(line -> {
+            try {
+                return DataMappingUtils.mapToRecord(line);
+            } catch (DataFormatException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(Collectors.toList());
 
         Collections.sort(records, comp);
 
-        FileWriter fw = new FileWriter(new File(targetPath));
-        BufferedWriter writer = new BufferedWriter(fw);
-        for (Record r : records) {
-            writer.write(r + "");
-        }
-        writer.close();
+        List<String> strList = records.stream().map(Record::toString).collect(Collectors.toList());
+
+        Files.write(Paths.get(targetPath), strList, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
     }
 }
--- src/io/github/accessun/largesort/operator/DataValidator.java
+++ src/io/github/accessun/largesort/operator/DataValidator.java
@@ -1,9 +1,9 @@
 package io.github.accessun.largesort.operator;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
 public class DataValidator {
@@ -12,40 +12,41 @@ public class DataValidator {
      * Validate the file that contains data waiting to be sorted. This function
      * should be called before the splitting and sorting operation to guarantee
      * that the data in the file can be correctly operated on by this program.
-     * 
+     *
      * This method reads in data from the file and validates them line by line.
      * It is expected that this method takes some time to process when the file
      * is quite large.
-     * 
+     *
      * @param pathname
      *            the pathname of the file to be validated
      * @return
      * @throws IOException
      */
     public boolean validate(String pathname) throws IOException {
-        FileReader fr = new FileReader(new File(pathname));
-        BufferedReader reader = new BufferedReader(fr);
-        String line;
         boolean result = true;
-        while ((line = reader.readLine()) != null) {
-            if (!validateLine(line)) {
-                result = false;
-                break;
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(pathname))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!validateLine(line)) {
+                    result = false;
+                    break;
+                }
             }
         }
-        reader.close();
+
         return result;
     }
 
     /**
      * Validate a line of text. If the line of text passed to this method
      * conforms to the specified pattern, the test is passed.
-     * 
+     *
      * The pattern against which a line of text is tested: one or more
      * alphanumeric characters followed by one or more tabs followed by two
      * digits. The first digit should be no smaller than 1 and no larger 8. The
      * second one should be within 0 and 9, inclusively.
-     * 
+     *
      * @param line
      * @return
      */
