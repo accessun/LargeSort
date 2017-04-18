a25f82cf6bbc3712c8cdd1e8f3055329474ab0d6
Xin Sun <accessun@hotmail.com>
2017-04-18 13:50:09 +0800

    added an overloaded method to DataMappingUtils

--- src/io/github/accessun/largesort/util/DataMappingUtils.java
+++ src/io/github/accessun/largesort/util/DataMappingUtils.java
@@ -6,24 +6,40 @@ import io.github.accessun.largesort.operator.DataValidator;
 
 public class DataMappingUtils {
 
-    private DataMappingUtils() {
-    }
+    private DataMappingUtils() {}
 
     /**
-     * Convert a string to a <tt>Record</tt> object. The string passed to this
-     * method should be a valid line of record in the original data file, or an
-     * exception will be thrown.
-     * 
+     * Convert a string to a {@code Record} object. The string passed to this
+     * method should be a valid line of record. Note that validation is
+     * performed before the mapping operation.
+     *
      * @param line
      *            a line of record in the original data file
      * @return
      * @throws DataFormatException
      */
     public static Record mapToRecord(String line) throws DataFormatException {
+        return mapToRecord(line, true);
+    }
+
+    /**
+     * Convert a string to a {@code Record} object. The string passed to this
+     * method should be a valid line of record.
+     *
+     * @param line
+     *            a string that corresponds to a Record
+     * @param enableValidation
+     *            enable validation or not
+     * @return
+     * @throws DataFormatException
+     */
+    public static Record mapToRecord(String line, boolean enableValidation) throws DataFormatException {
         DataValidator validator = new DataValidator();
-        if (!validator.validateLine(line)) {
+
+        if (enableValidation && !validator.validateLine(line)) {
             throw new DataFormatException("Map Failure! Re-check data format!");
         }
+
         String[] fields = line.split("\\t+");
         return new Record(fields[0], Integer.parseInt(fields[1]));
     }
