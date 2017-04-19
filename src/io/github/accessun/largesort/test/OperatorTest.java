ee87491f0b31a23c65fb6e62de557282041b9952
Xin Sun <accessun@hotmail.com>
2017-04-19 16:44:21 +0800

    added a method to perform clean-up operation

--- src/io/github/accessun/largesort/test/OperatorTest.java
+++ src/io/github/accessun/largesort/test/OperatorTest.java
@@ -1,15 +1,18 @@
 package io.github.accessun.largesort.test;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
-import io.github.accessun.largesort.handler.DataGeneratorHandler;
-import io.github.accessun.largesort.handler.FileSpliterHandler;
-import io.github.accessun.largesort.handler.LargeSortHandler;
 import io.github.accessun.largesort.model.MetaInfo;
 
 public class OperatorTest {
 
-    private MetaInfo info;
+    protected MetaInfo info;
 
     public OperatorTest() {
         int dataAmount = 30; // how many lines of data the file is intended to hold
@@ -17,19 +20,50 @@ public class OperatorTest {
         info = initializer.init(dataAmount);
     }
 
-    @Test
-    public void testGenerator() {
-        LargeSortHandler handler = new DataGeneratorHandler();
-        handler.handle(info);
+    /**
+     * <p>
+     * Delete all files and directories recursively in the base directory. The
+     * base directory is specified in the configuration file. A {@code boolean}
+     * parameter {@code delete} is expected to pass to this method. If the
+     * {@code delete} is {@code true}, this method is instructed to delete all
+     * the contents within the base directory. Otherwise, it simply print out
+     * all the files and directories that can be deleted. <strong>It is highly
+     * recommended that you should double check by passing {@code false} to this
+     * method before actually perform the clean-up.</strong>
+     *
+     * @param delete
+     *            whether to delete all the contents in the base directory
+     */
+    protected void cleanUp(boolean delete) {
+        Path start = Paths.get(info.getBaseDir());
+        try {
+            Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    cleanUpOperation(file, delete);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (!Files.isSameFile(dir, start))
+                        cleanUpOperation(dir, delete);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Test
-    public void testSpliter() {
-        LargeSortHandler handler = new DataGeneratorHandler();
-        LargeSortHandler spliterHandler = new FileSpliterHandler();
-        handler.setSuccessor(spliterHandler);
-
-        handler.handle(info);
+    private void cleanUpOperation(Path path, boolean delete) throws IOException {
+        String log = path.toString();
+        if (delete) {
+            log += " DELETED";
+            Files.delete(path);
+        }
+        System.out.println(log);
     }
 
     /*
