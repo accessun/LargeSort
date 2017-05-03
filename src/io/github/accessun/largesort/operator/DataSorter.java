c8ab0dccb7347c2e48dc9e582adec4615db3caaa
Xin Sun <accessun@hotmail.com>
2017-05-03 16:59:35 +0800

    modified sort method and added file check

--- src/io/github/accessun/largesort/operator/DataSorter.java
+++ src/io/github/accessun/largesort/operator/DataSorter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -45,7 +46,13 @@ public class DataSorter {
     }
 
     /**
-     * Sort the data in a file.
+     * Sort the data in a file. In the following three cases, this method does
+     * not perform sorting on the file:
+     * <ul>
+     * <li>File is empty</li>
+     * <li>File contains no more than one line of record</li>
+     * <li>The first line of the file is an empty line</li>
+     * </ul>
      *
      * @param pathname
      *            the path of the file to be sorted
@@ -60,18 +67,19 @@ public class DataSorter {
     public void sort(String pathname, String targetPath, Comparator<Record> comp)
             throws IOException, DataFormatException {
 
-        List<Record> records = Files.readAllLines(Paths.get(pathname)).stream().map(line -> {
-            try {
-                return DataMappingUtils.mapToRecord(line);
-            } catch (DataFormatException e) {
-                throw new RuntimeException(e);
-            }
-        }).collect(Collectors.toList());
+        List<String> lines = Files.readAllLines(Paths.get(pathname));
+        if (lines == null || lines.size() <= 1 || lines.get(0).trim().isEmpty())
+            return;
+
+        List<Record> records = new ArrayList<>();
+        for (String line : lines)
+            records.add(DataMappingUtils.mapToRecord(line));
 
         Collections.sort(records, comp);
 
         List<String> strList = records.stream().map(Record::toString).collect(Collectors.toList());
 
-        Files.write(Paths.get(targetPath), strList, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        // disallow file overwrite
+        Files.write(Paths.get(targetPath), strList, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
     }
 }
